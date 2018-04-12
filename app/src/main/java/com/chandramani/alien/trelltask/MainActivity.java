16624cd1dd4a01c9bc4fa4871fa739a758f4af5d
Chandramani <33385127+meChandramani@users.noreply.github.com>
2018-04-12 11:18:56 +0530

    second commit

--- app/src/main/java/com/chandramani/alien/trelltask/MainActivity.java
+++ app/src/main/java/com/chandramani/alien/trelltask/MainActivity.java
@@ -1,6 +1,7 @@
 package com.chandramani.alien.trelltask;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -13,11 +14,14 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.InflateException;
 import android.view.MotionEvent;
 import android.view.View;
@@ -25,31 +29,85 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TabHost;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
     private float dxx;
     private float dyy;
-    float valueofxx = 0;
+    float starttime,endtimee;
+    public int firstthumb,secondthumb;
+    public  float scrolled_valuex =0;
+    public HorizontalScrollView horizontalScrollView;
+    public float startthumb =0, endthumb=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ///////////////
+        horizontalScrollView = findViewById(R.id.horizontalscrollview1);
+        //toolbar
+        ///////////////////
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.arrow_toolbar_black);
+        //thumb drawing
+        /////////////////////
+        ImageView imagethumbview1 = findViewById(R.id.image1thumb);
+        ImageView imagethumbview2 = findViewById(R.id.image1thumb2);
+        Bitmap bb1 = Bitmap.createBitmap(80,300,Bitmap.Config.ARGB_8888);
+        Bitmap bb2 = Bitmap.createBitmap(80,300,Bitmap.Config.ARGB_8888);
+        Canvas canvas2 = new Canvas(bb1);
+        canvas2.setBitmap(bb1);
+        Paint paint3 =new Paint();
+        paint3.setStyle(Paint.Style.FILL);
+        paint3.setColor(Color.WHITE);
+        paint3.setStrokeWidth(10);
+        canvas2.drawCircle(imagethumbview1.getX()+40,150,40,paint3);
+        canvas2.drawLine(imagethumbview1.getX()+40,300,imagethumbview1.getX()+40,0,paint3);
+        canvas2.setBitmap(bb2);
+        canvas2.drawCircle(imagethumbview2.getX()+40,150,40,paint3);
+        canvas2.drawLine(imagethumbview2.getX()+40,300,imagethumbview2.getX()+40,0,paint3);
+        imagethumbview1.setImageBitmap(bb1);
+        imagethumbview2.setImageBitmap(bb2);
+        //rangebar set touch listner
+        //////////////////////////////
         View dragView = findViewById(R.id.image1thumb);
+        firstthumb = dragView.getId();
         dragView.setOnTouchListener(this);
         View dragView2 = findViewById(R.id.image1thumb2);
+        secondthumb = dragView2.getId();
         dragView2.setOnTouchListener(this);
-
-        //tabhostt
+        /////////////////////////////
+        /////////////////progessss initial
+        DisplayMetrics displayMetrics = getApplicationContext().getResources().getDisplayMetrics();
+        int width1= (int)((80 * displayMetrics.density) + 0.5);
+        int width2=(int)((250 * displayMetrics.density) + 0.5);
+        ImageView imagepogress =findViewById(R.id.progresspluss);
+        ImageView imagepogress2 =findViewById(R.id.progressminus);
+        RelativeLayout.LayoutParams layoutParamsx = new RelativeLayout.LayoutParams(width1+40,
+                RelativeLayout.LayoutParams.MATCH_PARENT
+        );
+        RelativeLayout.LayoutParams layoutParamsxx = new RelativeLayout.LayoutParams(Resources.getSystem().getDisplayMetrics().widthPixels-(width2+40),
+                RelativeLayout.LayoutParams.MATCH_PARENT
+        );
+        layoutParamsxx.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        imagepogress.setLayoutParams(layoutParamsx);                                            ///////
+        imagepogress2.setLayoutParams(layoutParamsxx);                                          ///////
+        startthumb = width1+40;
+        endthumb =(width2+40);
+                //tabhost
+        ////////////////////////////////
         TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
         tabHost.setup();
         TabHost.TabSpec tabSpec;
@@ -66,47 +124,69 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         tabSpec.setContent(R.id.tab12);
         tabHost.addTab(tabSpec);
         tabHost.setCurrentTab(1);
-
+        //video frames and uri
+        //////////////////////////////////
         final String uriPath = "android.resource://" + getPackageName() + "/raw/videoplayback";
         final Uri uri = Uri.parse(uriPath);
+        //MediaMetadataRetriver
+        //////////////////////////////////
         final MediaMetadataRetriever retriever = new MediaMetadataRetriever();
         retriever.setDataSource(getApplication(), uri);
-        //  retriever.setDataSource(this.path);
-        Bitmap bitmap = retriever.getFrameAtTime(5000);
+       // Bitmap bitmap = retriever.getFrameAtTime(5000);
         final VideoView imageView = findViewById(R.id.Imageframes);
         String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
         final long timeInMillisec = Long.parseLong(time);
         imageView.setVideoURI(uri);
         imageView.seekTo(1000);
-
         imageView.requestFocus();
         imageView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             public void onPrepared(MediaPlayer mp) {
                 imageView.pause();
+                mp.setVolume(0,0);
             }
         });
-        //for progresss
+        imageView.seekTo(1000);
+        ///////////////
+        final FrameLayout.LayoutParams layparm1 = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,FrameLayout.LayoutParams.MATCH_PARENT);
+        final FrameLayout.LayoutParams layparm2 = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        layparm2.gravity= Gravity.CENTER;
+        final ImageView imageViewPlayandPause = findViewById(R.id.playandpause);
 
+        imageViewPlayandPause.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                if(imageView.isPlaying()){
+                imageView.pause();
+                imageViewPlayandPause.setAlpha(255);
+               imageViewPlayandPause.setLayoutParams(layparm2);
+                }
+                else
+                {
+                    imageView.start();
+                    imageViewPlayandPause.setAlpha(0);
+                    imageViewPlayandPause.setLayoutParams(layparm1);
+                }
+
+            }
+        });
         //for scrollview frames
+        /////////////////////////////////
         final LinearLayout linearLayout = findViewById(R.id.imageviewframes2);
+
         final LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT
                 ,
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 1.0f
         );
-        final LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(100,
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                1.0f);
-
+        final LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(200,
+                LinearLayout.LayoutParams.MATCH_PARENT);
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < timeInMillisec * 1000; i += 2000000) {
+                for (int i = 0; i < timeInMillisec * 1000; i += 20000000) {
                     final ImageView imageView1 = new ImageButton(getBaseContext());
                     final Bitmap bitmap1 = retriever.getFrameAtTime(i);
 
-
-                    //imageView1.setLayoutParams(layoutParams);
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -124,40 +204,39 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 1.0f
         );
-        final LinearLayout.LayoutParams layoutParams4 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT
-                ,LinearLayout.LayoutParams.MATCH_PARENT,
+        int widtt = (int)((timeInMillisec*1000)/20000000)*200;
+        final LinearLayout.LayoutParams layoutParams4 = new LinearLayout.LayoutParams(widtt
+                , LinearLayout.LayoutParams.MATCH_PARENT,
                 1.0f
         );
+        // draw scale below the video frames
+        ///////////////////////////////////////////
         ImageView imageView3 = new ImageView(this);
         imageView3.setAdjustViewBounds(true);
-          Bitmap bb = Bitmap.createBitmap(15000,200,Bitmap.Config.ARGB_8888);
-          Canvas canvas=new Canvas(bb);
+        Bitmap bb = Bitmap.createBitmap(widtt+200,250,Bitmap.Config.ARGB_8888);
+        Canvas canvas=new Canvas(bb);
         canvas.setBitmap(bb);
         Paint paint = new Paint();
-        paint.setStrokeWidth(3);
+        paint.setStrokeWidth(2);
+        Paint paint2 = new Paint();
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.BLACK);
-        for (int i = 0; i < timeInMillisec; i+=20) {
-            if(i%100 ==0 || i==0) {
-                canvas.drawLine(i, 300, i, 50, paint);
+        paint2.setStyle(Paint.Style.FILL);
+        paint2.setTextSize(30f);
+        paint2.setColor(Color.BLACK);
+        paint2.setStrokeWidth(5);
+        for (int i =0; i < widtt+200; i+=20) {
+            if(i%200 ==0) {
+                canvas.drawLine(i, 300, i, 70, paint2);
                 String strr = new String();
-
-                canvas.drawText(strr.valueOf(i),i-5,30,paint);
-                Log.d("testt11","text");
+               canvas.drawText(strr.valueOf((i/200)*20),i-20,45,paint2);
             }
             else
-                canvas.drawLine(i,300,i,100,paint);
+                canvas.drawLine(i,300,i,130,paint);
         }
         imageView3.setImageBitmap(bb);
         linearLayout2.addView(imageView3,layoutParams4);
-
-
-
-
-
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -165,40 +244,58 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Toast.makeText(getApplicationContext(),"Trim \n Start_point:"+(valueofxx)*20+ "\n End_point:"+(valueofxx)*20,Toast.LENGTH_LONG).show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     public boolean onTouch(View view, MotionEvent event) {
+        ImageView imagepogress =findViewById(R.id.progresspluss);
+        ImageView imagepogress2 =findViewById(R.id.progressminus);
+        VideoView videoView = findViewById(R.id.Imageframes);
 
-        switch (event.getAction()) {
+         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 dxx = view.getX() - event.getRawX();
                 dyy = view.getY() - event.getRawY();
                 break;
             case MotionEvent.ACTION_MOVE:
                 view.setX(event.getRawX() + dxx);
-                valueofxx = view.getScrollX();
+                //rangebar progress
+                ////////////////////////////////////first thumb
+                if(view.getId()== firstthumb) {
+                   startthumb= (event.getRawX() + dxx+40);
+                   RelativeLayout.LayoutParams layoutParams2 = new RelativeLayout.LayoutParams((int)(event.getRawX()+dxx+40),
+                            RelativeLayout.LayoutParams.MATCH_PARENT
+                   );
+                   imagepogress.setLayoutParams(layoutParams2);
+                   videoView.seekTo((int)((startthumb/200)*20000));
+                }
+                ///////////////////second thumb
+                else if(view.getId()==secondthumb)
+                {
+                    endthumb = (event.getRawX()+dxx+40);
+                    RelativeLayout.LayoutParams layoutParams3 = new RelativeLayout.LayoutParams((Resources.getSystem().getDisplayMetrics().widthPixels)-(int)(event.getRawX()+dxx+40),
+                            RelativeLayout.LayoutParams.MATCH_PARENT
+                    );
+                    layoutParams3.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                    imagepogress2.setLayoutParams(layoutParams3);
+                    videoView.seekTo((int)((endthumb/200)*20000));
+                }
                 // view.setY(event.getRawY() + dyy);
                 break;
-
-
         }
         return true;
     }
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        DecimalFormat df = new DecimalFormat("#.000");
+       scrolled_valuex = horizontalScrollView.getScrollX();
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            starttime= ((scrolled_valuex+startthumb)/200)*20;
+            String starttimes =(df.format(starttime));
+            endtimee = ((scrolled_valuex+endthumb)/200)*20;
+            String endtimees =(df.format(endtimee));
+            Toast.makeText(getApplicationContext(),"Trim \n Start_point:"+starttimes+" Sec"+ "\n End_point:"+endtimees+" Sec",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
